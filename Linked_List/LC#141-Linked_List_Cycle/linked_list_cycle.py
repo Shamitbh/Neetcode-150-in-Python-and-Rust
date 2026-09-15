85741d0d231ced42f4feabfbbb9c97bd9508669c
Shamitbh <shamit.bhatia2@gmail.com>
2026-09-15 17:35:58 -0400

    Added implementation for LC 141 Linked List Cycle in Python

---
+++ Linked_List/LC#141-Linked_List_Cycle/linked_list_cycle.py
@@ -0,0 +1,24 @@
+# 141. Linked List Cycle
+# Difficulty: Easy
+# Topics: Hash Table, Linked List, Two Pointers, Floyd's Cycle Finding Algorithm
+# https://leetcode.com/problems/linked-list-cycle/
+
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        if not head:
+            return False
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
