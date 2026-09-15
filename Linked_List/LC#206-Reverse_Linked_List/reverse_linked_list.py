9b6c3bb3a8c5ed289ee1ba8b4d4147a720271cef
Shamitbh <shamit.bhatia2@gmail.com>
2026-09-15 00:19:53 -0400

    Added implementation for LC 206 Reverse Linked List in Python

---
+++ Linked_List/LC#206-Reverse_Linked_List/reverse_linked_list.py
@@ -0,0 +1,24 @@
+# 239. Reverse linked List
+# Difficulty: Easy
+# Topics: Linked List, Recursion
+# https://leetcode.com/problems/reverse-linked-list/
+
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
